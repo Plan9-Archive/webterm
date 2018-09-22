76c337d0b24d9cb37d380e276e92d967b9273a0a
eli <eli@cloud9>
2018-09-22 16:46:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Free HTML5 Desktop Environment
---
+++ loadui.js
@@ -0,0 +1,5 @@
+var s=document.createElement('script');s.src='ui.js';document.head.appendChild(s);
+s=document.createElement('script');s.src='extras.js';document.head.appendChild(s);
+var l=document.createElement('link');l.type='text/css';l.setAttribute('rel','stylesheet');l.href='ui.css';
+document.head.appendChild(l);
+//document.getElementById('terminal').style.display='none';
